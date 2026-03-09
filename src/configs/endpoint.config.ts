7746408ae188ac814498d462db0d7266aa93ffee
Noe <noelia2000vane@gmail.com>
2026-03-09 12:24:26 -0400

    FEAT #4: delete

--- src/configs/endpoint.config.ts
+++ src/configs/endpoint.config.ts
@@ -4,7 +4,6 @@ const endpointConfig = {
     signIn: '/auth/login',
     signOut: '/auth/logout',
     signUp: '/auth/register',
-
     forgotPassword: '/auth/forgot-password',
     resetPassword: '/auth/reset-password',
 }
