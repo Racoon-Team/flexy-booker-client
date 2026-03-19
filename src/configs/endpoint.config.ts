20538f7c0349459a5229205194fa0f4d934e51ce
Noe <noelia2000vane@gmail.com>
2026-03-18 23:03:17 -0400

    FEAT #11:change endpoint

--- src/configs/endpoint.config.ts
+++ src/configs/endpoint.config.ts
@@ -1,11 +1,11 @@
 export const apiPrefix = 'http://localhost:3000/api'
 
 const endpointConfig = {
-    signIn: '/auth/sign-in',
-    signOut: '/auth/sign-out',
-    signUp: '/auth/sign-up',
-    forgotPassword: '/auth/forgot-password',
-    resetPassword: '/auth/reset-password',
+    signIn: '/users/sign-in',
+    signOut: '/users/sign-out',
+    signUp: '/users/sign-up',
+    forgotPassword: '/users/forgot-password',
+    resetPassword: '/users/reset-password',
 }
 
 export default endpointConfig
