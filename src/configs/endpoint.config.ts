dc415bd1af77e802ab06fc647e551d810ae254dc
Noe <noelia2000vane@gmail.com>
2026-03-24 17:37:48 -0400

    FEAT #11:change of route

--- src/configs/endpoint.config.ts
+++ src/configs/endpoint.config.ts
@@ -1,11 +1,11 @@
 export const apiPrefix = 'http://localhost:3000/api'
 
 const endpointConfig = {
-    signIn: '/users/sign-in',
-    signOut: '/users/sign-out',
-    signUp: '/users/sign-up',
-    forgotPassword: '/users/forgot-password',
-    resetPassword: '/users/reset-password',
+    signIn: '/auth/login',
+    signOut: '/auth/logout',
+    signUp: '/auth/register',
+    forgotPassword: '/auth/forgot-password',
+    resetPassword: '/auth/reset-password',
 }
 
 export default endpointConfig
